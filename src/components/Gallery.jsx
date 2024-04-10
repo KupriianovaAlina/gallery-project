cb5d71f17ba1f0c4d1dfdfaa00831da5edfc198d
VitaFilimonova <114240442+VitaFilimonova@users.noreply.github.com>
2024-04-11 00:54:16 +0400

    ASTON-REACT [fear]: add loader

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,28 @@
 import CardPreview from './CardPreview';
 import { useSelector } from 'react-redux';
 import { charactersSelector } from '../slices/selectors';
+import { useEffect, useState } from 'react';
 
-const Gallery = () => {
+const Gallery = ({ setImagesLoading }) => {
+  const [loadedImagesCount, setLoadedImagesCount] = useState(0);
   const characters = useSelector(charactersSelector);
 
+  useEffect(() => {
+    if (Object.values(characters.byIds).length === 0) {
+      setImagesLoading(false);
+    }
+  }, [characters.byIds, setImagesLoading]);
+
+  const handleImageLoaded = () => {
+    setLoadedImagesCount(prevCount => {
+      const updatedCount = prevCount + 1;
+      // Если все изображения загружены, обновляем состояние в MainPage
+      if (updatedCount === Object.values(characters.byIds).length) {
+        setImagesLoading(false);
+      }
+      return updatedCount;
+    });
+  };
   if (
     Object.keys(characters.byIds).length === 0 ||
     characters.fetchStatus === 'rejected'
@@ -19,7 +37,13 @@ const Gallery = () => {
   return (
     <section className="flex flex-wrap gap-10 px-10 justify-center">
       {Object.values(characters.byIds).map(character => {
-        return <CardPreview character={character} key={character.id} />;
+        return (
+          <CardPreview
+            character={character}
+            key={character.id}
+            onImageLoaded={() => handleImageLoaded()}
+          />
+        );
       })}
     </section>
   );
